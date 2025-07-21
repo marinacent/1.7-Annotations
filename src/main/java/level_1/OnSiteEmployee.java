ee7a6dac3fdbab03cf64fae42620138a91cc32ee
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-21 11:01:12 +0200

    feat: override compute salary method in on-site employee class

--- src/main/java/level_1/OnSiteEmployee.java
+++ src/main/java/level_1/OnSiteEmployee.java
@@ -1,9 +1,15 @@
 package level_1;
 
 public class OnSiteEmployee extends Employee {
+    private static int petrol = 140;
 
     public OnSiteEmployee(String name, String surname, double hourlyWage) {
         super(name, surname, hourlyWage);
+    }
+
+    @Override
+    public double computeSalary(int hoursWorked) {
+        return super.computeSalary((hoursWorked)) + petrol;
     }
 
 }
