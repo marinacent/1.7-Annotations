7020b1a6f7fe42cd99f9827b7e89e758ae4be2a8
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-21 11:08:14 +0200

    feat: override compute salary method for remote workers

--- src/main/java/level_1/RemoteEmployee.java
+++ src/main/java/level_1/RemoteEmployee.java
@@ -1,10 +1,15 @@
 package level_1;
 
 public class RemoteEmployee extends Employee {
+    public static final int INTERNET_FLAT_RATE = 40;
 
     public RemoteEmployee(String name, String surname, double hourlyWage) {
         super(name, surname, hourlyWage);
     }
 
-    
+    @Override
+    public double computeSalary(int hoursWorked) {
+        return super.computeSalary(hoursWorked) + INTERNET_FLAT_RATE;
+    }
+
 }
