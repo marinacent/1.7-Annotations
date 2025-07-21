b632dcae0fd283e16f8c1b6f995baa7e32c37793
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-21 13:41:21 +0200

    feat: add jackson dependencies to pom

---
+++ src/main/java/level_2/JsonSerializer.java
@@ -0,0 +1,10 @@
+package level_2;
+
+public class JsonSerializer {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    public static void serialize(Object object) {
+
+    }
+
+}
