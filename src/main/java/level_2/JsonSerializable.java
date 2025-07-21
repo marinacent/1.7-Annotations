684b1024a82b0251c25c950b4469bf2e2bee0c1f
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-21 15:05:11 +0200

    feat: create custom json serializable annotation

---
+++ src/main/java/level_2/JsonSerializable.java
@@ -0,0 +1,12 @@
+package level_2;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface JsonSerializable {
+    String directory();
+}
