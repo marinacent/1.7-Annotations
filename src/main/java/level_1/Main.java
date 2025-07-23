075a62d1cb28e7040a534bb5dd5ec0840e0c70fa
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-23 12:22:35 +0200

    style: use camel case

--- src/main/java/level_1/Main.java
+++ src/main/java/level_1/Main.java
@@ -3,17 +3,17 @@ package level_1;
 public class Main {
 
     public static void main(String[] args) {
-        int MONTHLY_HOURS = 160;
+        int monthlyHours = 160;
 
         Employee sophia = new Employee("Sophia", "Garcia", 20);
         OnSiteEmployee joan = new OnSiteEmployee("Joan", "Pastor", 20);
         RemoteEmployee irene = new RemoteEmployee("Irene", "Vives", 20);
 
-        System.out.println(sophia.computeSalary(MONTHLY_HOURS));
-        System.out.println(joan.computeSalary(MONTHLY_HOURS));
-        System.out.println(irene.computeSalary(MONTHLY_HOURS));
+        System.out.println(sophia.computeSalary(monthlyHours));
+        System.out.println(joan.computeSalary(monthlyHours));
+        System.out.println(irene.computeSalary(monthlyHours));
 
-        System.out.println(DeprecatedMethodsCaller.callComputeCoffeeFee(joan, MONTHLY_HOURS));
-        System.out.println(DeprecatedMethodsCaller.callComputeDaysInOffice(irene, MONTHLY_HOURS));
+        System.out.println(DeprecatedMethodsCaller.callComputeCoffeeFee(joan, monthlyHours));
+        System.out.println(DeprecatedMethodsCaller.callComputeDaysInOffice(irene, monthlyHours));
     }
 }
